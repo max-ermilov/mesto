b7d9e47801b22cb716e8631e5ef0d273c0e20887
Maxim Ermilov <max-ermilov@yandex.ru>
2022-03-10 16:36:12 +0200

    refactor(rf/s8add_user_info_class): add UserInfo  class

---
+++ scripts/UserInfo.js
@@ -0,0 +1,18 @@
+export default class UserInfo {
+  constructor(userNameSelector, userJobSelector) {
+    this._name = document.querySelector(userNameSelector);
+    this._job = document.querySelector(userJobSelector);
+  }
+
+  getUserInfo() {
+    return {
+      name: this._name.textContent,
+      profession: this._job.textContent
+    }
+  }
+
+  setUserInfo(nameInput, jobInput) {
+    this._name.textContent = nameInput;
+    this._job.textContent = jobInput;
+  }
+}
